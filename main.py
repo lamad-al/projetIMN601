7a64d70c4f78efaac435350721c04a0227ffc768
Jean-Philippe Rochette <jp.roch9@gmail.com>
2016-12-08 10:28:49 -0500

    Correction d'une erreur dans les dictio de feature. Ajout d'un feature.

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Images:
         self.training_labels = self.training_and_validation_labels[nb_validation_samples:]
 
         # Print a cute little message
+        # TODO : Faire en sorte de pouvoir afficher plusieurs noms si on ajoute des db
         print("Loaded {} training data, {} validation data and {} test data for {}."
               .format(len(self.training_data),
                       len(self.validation_data),
@@ -49,14 +50,14 @@ class Images:
 
         :param data_set: The type of data set ('training', 'validation' or 'test')
         :type data_set: str
-        :param feature: The type of feature to extract ('gray_scale')
+        :param feature: The type of feature to extract ('gray_scale', 'raw_pixels', 'rgb')
         :type feature: str
         :return: A list with one list of feature for every image in the data set
         :rtype: list
         """
         # Check our input
         assert data_set in ('training', 'validation', 'test')
-        assert feature in ('gray_scale', 'raw_pixels')
+        assert feature in ('gray_scale', 'raw_pixels', 'rgb')
 
         # Choose the good data set
         data = {
@@ -66,10 +67,12 @@ class Images:
         }[data_set]
 
         # Extract the features
-        result = {
-            'gray_scale': self.gray_scale(data),
-            'raw_pixels': self.raw_pixels(data)
+        function = {
+            'gray_scale': self.gray_scale,
+            'raw_pixels': self.raw_pixels,
+            'rgb': self.rgb
         }[feature]
+        result = function(data)
 
         return result
 
@@ -128,6 +131,13 @@ class Images:
         return gs_data
 
     def raw_pixels(self, all_img):
+        """Every image becomes a list of raw pixels (no other transformation).
+
+        For RGB images, for a pixel p, the representation is: p[1].red, p[1].green, p[1].blue, p[2].red, ...
+
+        :param all_img: A data set returned by "keras.datasets"
+        :return: A list with one list of feature (one pixel after the other) for every image.
+        """
         gs_data = []
         for img in all_img:
             new_img = []
@@ -142,45 +152,69 @@ class Images:
             gs_data.append(new_img)
         return gs_data
 
+    def rgb(self, all_img):
+        """Only for 3D images. Each image is represented by its red channel, then green channel, and its blue at the end.
+
+        :param all_img: A data set returned by "keras.datasets" (3D)
+        :return: A list with one list of feature (RGB) for every image.
+        """
+        gs_data = []
+        for img in all_img:
+            red_channel = []
+            green_channel = []
+            blue_channel = []
+            for line in img:
+                for RGB in line:
+                    red_channel.append(RGB[0])
+                    green_channel.append(RGB[1])
+                    blue_channel.append(RGB[2])
+            gs_data.append(red_channel + green_channel + blue_channel)
+        return gs_data
 
 ########################################################################################################################
 #                                               Main
 ########################################################################################################################
 if __name__ == '__main__':
     # TODO : Automatiser un peu plus le tout. Là il faut changer plusieurs paramètres et mots manuellement...
-    images = Images(database=cifar10, slice=0.1)
+    # Choose which databases to use for the simulation
+    #db_list = (mnist, cifar10)
+    db_list = (mnist,)
+    #db_list = (cifar10,)
 
-    # Get the training data set with its labels
-    X = images.get_data_set(data_set="training", feature="raw_pixels")
-    Y = images.get_labels(data_set="training")
+    for db in db_list:
+        images = Images(database=db, slice=0.1)
 
-    # Get the validation data set with its labels
-    V = images.get_data_set(data_set="validation", feature="raw_pixels")
-    W = images.get_labels(data_set="validation")
+        # Get the training data set with its labels
+        X = images.get_data_set(data_set="training", feature="raw_pixels")
+        Y = images.get_labels(data_set="training")
 
-    # Get the parameters with Grid Search
-    # TODO : Faire une liste des paramètres à trouver et setter
-    # TODO : Implémenter un Grid Search (regarder si Keras en a pas un, ça serait trop nice...)
+        # Get the validation data set with its labels
+        V = images.get_data_set(data_set="validation", feature="raw_pixels")
+        W = images.get_labels(data_set="validation")
 
-    # Train the classifier
-    # TODO : Ajout d'un autre algorithme et d'un autre feature
-    # TODO : Utiliser nos paramètres trouvés par Grid Search
-    classifier = SGDClassifier(loss='log', alpha=0.001, learning_rate='optimal', eta0=1, n_iter=1000)
-    clf = classifier.fit(X, Y)
+        # Get the parameters with Grid Search
+        # TODO : Faire une liste des paramètres à trouver et setter
+        # TODO : Implémenter un Grid Search (regarder si Keras en a pas un, ça serait trop nice...)
 
-    # Get the accuracy for the training data set
-    print('Logistic regression training accuracy = ', clf.score(X, Y) * 100, '%')
+        # Train the classifier
+        # TODO : Ajout d'un autre algorithme et d'un autre feature
+        # TODO : Utiliser nos paramètres trouvés par Grid Search
+        classifier = SGDClassifier(loss='log', alpha=0.001, learning_rate='optimal', eta0=1, n_iter=1000)
+        clf = classifier.fit(X, Y)
 
-    # Get the test data set with its labels
-    X = images.get_data_set(data_set="test", feature="raw_pixels")
-    Y = images.get_labels(data_set="test")
+        # Get the accuracy for the training data set
+        print('Logistic regression training accuracy = ', clf.score(X, Y) * 100, '%')
 
-    # Get the accuracy for the test data set
-    print('Logistic regression test accuracy = ', clf.score(X, Y) * 100, '%')
+        # Get the test data set with its labels
+        X = images.get_data_set(data_set="test", feature="raw_pixels")
+        Y = images.get_labels(data_set="test")
 
-    # Print a confusion matrix
-    cm = confusion_matrix(Y, clf.predict(X))
-    print(cm)
+        # Get the accuracy for the test data set
+        print('Logistic regression test accuracy = ', clf.score(X, Y) * 100, '%')
+
+        # Print a confusion matrix
+        cm = confusion_matrix(Y, clf.predict(X))
+        print(cm)
 
 
 
